564df055684a02f62c437997cd7f584ddea619bd
israr ahmad <israr2800@gmail.com>
2023-02-09 23:59:20 +0530

    jquery component

---
+++ Other effects/dataTable-grid-advanced/js/myscript.js
@@ -0,0 +1,158 @@
+$(document).ready(function () {
+	$("div").removeClass("tableLoad");
+})
+// all hideshow
+function hideShowToggle(toggleClass){
+	$("." + toggleClass).stop().toggle();
+}
+// all hideshow end
+
+// <!-- ====================LEFT PANEL ON CLICK==================== -->
+$(document).ready(function(){
+  $('.clickButton').click(function(){
+		$(this).toggleClass('position-inherit',500);
+		$(".leftPanel").hide(0);
+	  $('.leftPanelGrid').stop().toggle(500);
+	});
+	$('.hidePanelicon').click(function(){
+	  $('.leftPanelGrid').hide(500);
+	});
+});
+// <!-- ====================LEFT PANEL ON CLICK END ==================== -->
+
+// <!-- ====================  TABLE GRID DATA TABLE ==================== -->
+
+// var globDataTable;
+
+$(document).ready(function(){
+	
+	globDataTable = $('#example').DataTable({
+		"scrollY": "300px",
+		"scrollX": true
+	});
+	$('#example').DataTable().draw();
+});
+
+function filterGlobal () {
+	$('#example').DataTable().search(
+			$('#global_filter').val(),
+			$('#global_regex').prop('checked'),
+			$('#global_smart').prop('checked')
+	).draw();
+}
+
+function filterColumn ( i ) {
+	$('#example').DataTable().column( i ).search(
+			$('#col' + i + '_filter').val(),
+			$('#col' + i + '_regex').prop('checked'),
+			$('#col' + i + '_smart').prop('checked')
+	).draw();
+}
+
+$(document).ready(function() {
+	$("#appynowButton").click(function(){ 
+
+		globDataTable.search($("input.column_filter").val()).draw();  // add to search on click
+
+	// $('input.global_filter').on( 'keyup click', function () { " use on instead prop "
+	// 		filterGlobal();
+	// } );
+
+	$('input.column_filter').prop( 'keyup click', function () {
+			filterColumn( $(this).parents('tr').attr('data-column') );
+	} );
+});
+});
+
+
+// toggle dataTable specific column
+$(".nameToggle").click(function () {
+	var col = globDataTable.column(0);
+	col.visible(!col.visible());
+})
+$(".positionToggle").click(function () {
+	var col = globDataTable.column(1);
+	col.visible(!col.visible());
+})
+$(".officeToggle").click(function () {
+	var col = globDataTable.column(2);
+	col.visible(!col.visible());
+})
+$(".ageToggle").click(function () {
+	var col = globDataTable.column(3);
+	col.visible(!col.visible());
+})
+$(".startdateToggle").click(function () {
+	var col = globDataTable.column(4);
+	col.visible(!col.visible());
+})
+$(".salaryToggle").click(function () {
+	var col = globDataTable.column(5);
+	col.visible(!col.visible());
+})
+// toggle dataTable specific column end
+
+// <!-- ====================  TABLE GRID DATA TABLE END ==================== -->
+
+// search left table
+function myFunction() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+// search left table end
+
+// voide dropbox close
+$(document).on('click', '.preventDropboxClose', function (e) {
+  e.stopPropagation();
+});	
+// voide dropbox close end
+
+
+// auto arrange unchecked input box
+$(document).ready(function(){
+	var list = $(".autoArrangeUl");
+	sortItems(list);
+	var anch = $('#clear');
+$('#clear').click(function(){   
+	var list = $(".autoArrangeUl"); $(this).parents().eq(0).find(':checkbox').removeAttr('checked');
+													 sortItems(list);
+});
+function sortItems(list){
+	$(":checkbox").attr("autocomplete", "off");
+	 origOrder = list.children();
+	list.on("click", ":checkbox", function() {
+			var i, checked = document.createDocumentFragment(),
+					unchecked = document.createDocumentFragment();
+			for (i = 2; i < origOrder.length; i++) {
+					if (origOrder[i].getElementsByTagName("input")[0].checked) {
+							checked.appendChild(origOrder[i]);
+					} else {
+							unchecked.appendChild(origOrder[i]);
+					}
+			}
+			list.append(checked).append(unchecked);
+	});
+}
+});
+// auto arrange unchecked input box end
+
+// hide to apply button when no input is checked
+	var boxes = $('.mycheckBox');
+
+	boxes.on('change', function () {
+	$('#appynowButton').toggleClass('defaultHide', !boxes.filter(':checked').length);
+	}).trigger('change');
+// hide to apply button when no input is checked
---
+++ Other effects/dataTable-grid-multiSelectOption/js/datatabel/datatable-myscript.js
@@ -0,0 +1,106 @@
+﻿$(document).ready(function() {
+    // showing data table after full load
+    $('#example').show();
+    // showing data table after full load end
+
+    var table = $('#example').DataTable({ // add "var table" for multiselect
+        "scrollY": 300,
+        "scrollX": true,
+        "ordering": false, 
+        // "lengthChange": true,
+
+        // multi select filter with select2
+        initComplete: function () {
+            count = 0;
+            this.api().columns().every( function () {
+                var title = this.header();
+                //replace spaces with dashes
+                title = $(title).html().replace(/[\W]/g, '-');
+                var column = this;
+                var select = $('<select id="' + title + '" class="select2" ></select>')
+                    .appendTo( $(column.header()).empty() )
+                    .on( 'change', function () {
+                      //Get the "text" property from each selected data 
+                      //regex escape the value and store in array
+                      var data = $.map( $(this).select2('data'), function( value, key ) {
+                        return value.text ? '^' + $.fn.dataTable.util.escapeRegex(value.text) + '$' : null;
+                                 });
+                      
+                      //if no data selected use ""
+                      if (data.length === 0) {
+                        data = [""];
+                      }
+                      
+                      //join array into string with regex or (|)
+                      var val = data.join('|');
+                      
+                      //search for the option(s) selected
+                      column
+                            .search( val ? val : '', true, false )
+                            .draw();
+                    } );
+ 
+                column.data().unique().sort().each( function ( d, j ) {
+                    select.append( '<option value="'+d+'">'+d+'</option>' );
+                } );
+              
+              //use column title as selector and placeholder
+              $('#' + title).select2({
+                multiple: true,
+                closeOnSelect: false,
+                placeholder: "Select a " + title
+              });
+              
+              //initially clear select otherwise first option is selected
+              $('.select2').val(null).trigger('change');
+            } );
+        },
+
+        // multi select filter with select2 end
+
+        // for the copy excel psd etc buttons
+        dom: 'Bfrtip',
+
+        buttons: [{
+                extend: 'copy',
+                exportOptions: {
+                    columns: ':visible'
+                }
+            },
+            {
+                extend: 'csv',
+                exportOptions: {
+                    columns: ':visible'
+
+                }
+            },
+            {
+                extend: 'excel',
+                exportOptions: {
+                    columns: ':visible',
+                    columns: "thead th:not(.noExport)" // to not download first column
+                }
+            },
+            {
+                extend: 'pdf',
+                exportOptions: {
+                    columns: ':visible'
+                }
+            },
+            {
+                extend: 'print',
+                exportOptions: {
+                    columns: ':visible'
+                }
+            },
+            'colvis',
+            'pageLength' // page length enable disabled
+        ],
+        columnDefs: [{
+                targets: 0,
+                visible: true
+            }]
+            // for the copy excel psd etc buttons end
+
+    });
+});
